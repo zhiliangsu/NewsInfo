1942e5e55c47982cf152077cdf86863926208a71
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-01-03 22:26:34 +0800

    点击排行展示前后端实现

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,5 @@
-from info.models import User
+from info import constants
+from info.models import User, News
 from info.modules.index import index_bp
 from flask import current_app, render_template, session, jsonify
 
@@ -7,7 +8,7 @@ from info.utils.response_code import RET
 
 @index_bp.route('/')
 def index():
-    # -----------------用户登录成功,查询用户基本信息展示---------------
+    # -----------------1.用户登录成功,查询用户基本信息展示---------------
     # 1. 获取用户的id
     user_id = session.get("user_id")
     # 先声明防止局部变量不能访问
@@ -27,6 +28,18 @@ def index():
     """
     user_dict = user.to_dict() if user else None
 
+    # -----------------2.查询新闻点击排行数据展示---------------
+    try:
+        news_rank_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询新闻对象异常")
+
+    news_dict_list = []
+    # 将新闻列表对象列表转成字典列表
+    for news in news_rank_list if news_rank_list else []:
+        news_dict_list.append(news.to_dict())
+
     # 4. 组织响应数据
     """
     数据格式:
@@ -39,7 +52,8 @@ def index():
     前端使用方式: data.user_info.nick_name
     """
     data = {
-        "user_info": user_dict
+        "user_info": user_dict,
+        "click_news_list": news_dict_list
     }
 
     return render_template("news/index.html", data=data)
