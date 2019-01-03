ee2096061be92a5abc24c8dd36a749e7187bfad2
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-01-03 19:13:51 +0800

    登录成功首页右上角内容逻辑显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,12 +1,53 @@
+from info.models import User
 from info.modules.index import index_bp
-from flask import current_app, render_template
+from flask import current_app, render_template, session, jsonify
+
+from info.utils.response_code import RET
 
 
 @index_bp.route('/')
 def index():
-    # 返回渲染模板文件
-    print(current_app.url_map)
-    return render_template("news/index.html")
+    # -----------------用户登录成功,查询用户基本信息展示---------------
+    # 1. 获取用户的id
+    user_id = session.get("user_id")
+    # 先声明防止局部变量不能访问
+    user = None
+    if user_id:
+        # 2. 根据user_id查询当前登录的用户对象
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(errno=RET.DBERR, errmsg="查询用户对象异常")
+
+    # 3. 将用户对象转成字典
+    """
+    if user:
+        user_dict = user.to_dict()
+    """
+    user_dict = user.to_dict() if user else None
+
+    # 4. 组织响应数据
+    """
+    数据格式:
+    data = {
+        "user_info": {
+            "id": self.id,
+            "nick_name": self.nick_name
+        }
+    }
+    前端使用方式: data.user_info.nick_name
+    """
+    data = {
+        "user_info": user_dict
+    }
+    test = {
+        "id": "003",
+        "name": "curry",
+        "number": "30"
+    }
+
+    return render_template("news/index.html", data=data, test=test)
 
 
 @index_bp.route('/favicon.ico')
