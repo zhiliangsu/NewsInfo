31508876ebc823c7e4fdeaf51bae6cfc9f00d506
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-01-04 14:10:24 +0800

    基类模板抽取

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -6,4 +6,5 @@ from flask import render_template
 @news_bp.route('/<int:news_id>')
 def news_detail(news_id):
     """新闻详情页展示"""
-    return render_template("news/detail.html")
+    data = {}
+    return render_template("news/detail.html", data=data)
