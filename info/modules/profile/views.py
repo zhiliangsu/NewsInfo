a3c95c096f09c8152f90f132bf3ee2c025d4e449
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-01-05 22:01:37 +0800

    查询用户基本资料并展示

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,6 +1,24 @@
-from flask import g, render_template
+from flask import g, render_template, request
 from info import get_user_data
 from . import profile_bp
+
+
+# 127.0.0.1:5000/user/baseinfo ---> 用户基本资料页面
+@profile_bp.route('/baseinfo', methods=["POST", "GET"])
+@get_user_data
+def baseinfo():
+    """用户基本资料页面"""
+
+    # GET请求: 返回模板页面,展示用户基本资料
+    if request.method == "GET":
+        user = g.user
+        data = {
+            "user_info": user.to_dict() if user else None
+        }
+
+        return render_template("profile/user_base_info.html", data=data)
+
+    # POST请求: 修改用户基本资料
 
 
 # 127.0.0.1:5000/user/info ---> 个人中心页面
@@ -13,4 +31,3 @@ def user_info():
         "user_info": user.to_dict() if user else None
     }
     return render_template("profile/user.html", data=data)
-
