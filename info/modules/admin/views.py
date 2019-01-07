9d5cad9f6b5df69da0a63035bb172192cf57e7c5
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-01-07 17:32:07 +0800

    管理员用户的登录逻辑实现&返回管理员首页

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,5 +1,15 @@
-from flask import request, render_template
+from flask import request, render_template, jsonify, current_app, session, redirect, url_for
+from info.models import User
+from info.utils.response_code import RET
 from . import admin_bp
+
+
+# 127.0.0.1:5000/admin/
+@admin_bp.route('/')
+def admin_index():
+    """管理员首页"""
+    # 返回首页模板
+    return render_template("admin/index.html")
 
 
 # 127.0.0.1:5000/admin/login
@@ -12,3 +22,45 @@ def admin_login():
         return render_template("admin/login.html")
 
     # POST请求: 后台登录的逻辑处理
+    """
+    1.获取参数
+        1.1 username:管理员用户账号，password：未加密密码
+    2.校验参数
+        2.1 非空判断
+    3.逻辑处理
+        3.0 根据name账号查询出管理员用户对象
+        3.1 管理员用户的密码校验
+        3.2 使用session记录管理员登录信息
+    4.返回值
+        登录成功
+    """
+
+    # 获取参数
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    # 校验参数
+    if not all([username, password]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不足")
+
+    # 逻辑处理
+    admin_user = None  # type: User
+    try:
+        admin_user = User.query.filter(User.is_admin == True, User.mobile == username).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询管理员用户异常")
+
+    if not admin_user:
+        return render_template("admin/login.html", errmsg="管理员用户不存在")
+
+    # 使用session记录管理员登录信息
+    session["user_id"] = admin_user.id
+    session["mobile"] = admin_user.mobile
+    session["nick_name"] = username
+    # 记录用户对象是否是管理员
+    session["is_admin"] = True
+
+    # 转到管理员首页
+    return redirect(url_for("admin.admin_index"))
+
